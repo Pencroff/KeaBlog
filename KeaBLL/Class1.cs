a75677ae2d2ef8fc50eba51a2beb225fd8a8393d
Pencroff <pencroff@gmail.com>
2012-06-25 02:14:23 +0300

    KeaBlog first commit
    Blog (MVC4)
    BLL
    DAL
    Connect to base EF 4.3

---
+++ KeaBLL/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeaBLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ KeaBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KeaBlog.MvcApplication" Language="C#" %>
