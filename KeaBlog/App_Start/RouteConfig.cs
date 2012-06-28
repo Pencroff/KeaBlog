46e9050438d21dc35d17a6136e303a5035049212
Pencroff <pencroff@gmail.com>
2012-06-28 22:50:38 +0300

    Add area "Admin"
    Create routing to area and main part

--- KeaBlog/App_Start/RouteConfig.cs
+++ KeaBlog/App_Start/RouteConfig.cs
@@ -23,8 +23,10 @@ namespace KeaBlog
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "KeaBlog.Controllers" }
             );
+            //ControllerBuilder.Current.DefaultNamespaces.Add("KeaBlog.Controllers");
         }
     }
 }
