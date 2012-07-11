5c4ec9629e62b8b1ffe892baed2d10ab46f04e27
Pencroff <pencroff@gmail.com>
2012-07-11 23:26:31 +0300

    Correct commit with commit from internet

--- KeaBLL/PostManager.cs
+++ KeaBLL/PostManager.cs
@@ -8,7 +8,7 @@ namespace KeaBLL
 {
     public static class PostManager
     {
-        public static PostAuthor GetPostAuthorById(int postId)
+        public static PostAuthor GetPostById(int postId)
         {
             PostAuthor result = null;
             using (KeaContext context = new KeaContext())
@@ -18,12 +18,16 @@ namespace KeaBLL
             return result;
         }
 
-        public static List<PostAuthor> GetPostAutorListByPage(int startPageIndex, int endPageIndex)
+        public static List<PostAuthor> GetPostListByPage(int startPageIndex, int endPageIndex, out int postCount)
         {
-            List<PostAuthor> result = null;
+            List<PostAuthor> result = new List<PostAuthor>();
             using (KeaContext context = new KeaContext())
             {
-                result = context.PostAuthorByPage(startPageIndex, endPageIndex).ToList();
+                postCount = 0;
+                //result = context.PostAuthorByPage(startPageIndex, endPageIndex).ToList();
+                //ObjectParameter cnt = new ObjectParameter("count", typeof(int));
+                //result = context.SearchAuthorShortPagingGet(query, startIndex, endIndex, cnt).ToList();
+                //authorCount = Convert.ToInt32(cnt.Value);
             }
             return result;
         }
