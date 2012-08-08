7f72cabe18a076e6093653e739719844730b7da0
Pencroff <pencroff@gmail.com>
2012-08-08 18:50:55 +0300

    Add sp_PostTagsInsert - many to many relation
    Add Split.function and SplitXML.function for example

---
+++ Database.Kea/SplitXML.function.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[SplitXML] (@sep VARCHAR(32), @s VARCHAR(MAX))
+
+RETURNS @t TABLE
+    (
+        val VARCHAR(MAX)
+    )   
+AS
+    BEGIN
+        DECLARE @xml XML
+        SET @XML = N'<root><r>' + REPLACE(@s, @sep, '</r><r>') + '</r></root>'
+
+        INSERT INTO @t(val)
+        SELECT r.value('.','VARCHAR(5)') as Item
+        FROM @xml.nodes('//root/r') AS RECORDS(r)
+
+        RETURN
+    END
---
+++ Database.Kea/sp_PostTagsInsert.proc.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[sp_PostTagsInsert](
+    @postId INT,
+    @tags nvarchar(256)
+)
+AS
+BEGIN
+	DECLARE @sql nvarchar(max)
+	SET @sql = 'SELECT ' + CAST(@postId AS nvarchar(10)) +', t.Id FROM dbo.Tags t WHERE t.Id IN (' + @tags +')'
+	INSERT INTO [dbo].PostsInTags EXEC sp_executesql @sql
+END
