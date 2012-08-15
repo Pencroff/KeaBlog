c10f80f98a26845f2ed1e0c1f70e1b191b9228b4
Pencroff <pencroff@gmail.com>
2012-08-16 00:34:40 +0300

    Add Search controller and View

---
+++ KeaBlog/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KeaBlog.Areas.Admin.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index(string query)
+        {
+            if (String.IsNullOrEmpty(query))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
