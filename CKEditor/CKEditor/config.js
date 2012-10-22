76fb71d190538c27d7a4e95b49b60047d4e6c9ff
Pencroff <pencroff@gmail.com>
2012-10-23 00:46:13 +0300

    Add plagins: autocss, htmlbuttons, zoom
    Update config.js

--- CKEditor/CKEditor/config.js
+++ CKEditor/CKEditor/config.js
@@ -8,9 +8,32 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-    config.extraPlugins = 'syntaxhighlight';
-//    config.toolbar =
-//        [
-//            [ 'Source', '-', 'Bold', 'Italic', 'syntaxhighlight' ]
-//        ];
+    config.extraPlugins = 'syntaxhighlight,zoom,autocss,htmlbuttons';
+    config.toolbar =
+        [
+            { name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+            { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+            { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
+            { name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
+                'HiddenField' ] },
+            '/',
+            { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+            { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+                '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+            { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+            { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+            '/',
+            { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+            { name: 'colors', items : [ 'TextColor','BGColor' ] },
+            { name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] },
+            { name: 'customTools', items : [ 'CutBtn', 'syntaxhighlight', 'Zoom' ] }
+        ];
+    config.htmlbuttons = [
+        {
+            name:'CutBtn',
+            icon:'cut_red.png',
+            html:'[cut]',
+            title:'Read more'
+        }
+    ];
 };
